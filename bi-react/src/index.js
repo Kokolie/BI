2d35d9eebc34a37d1fcf1014a5af38bf4cce0619
Kokolie <ladylilith94@hotmail.com>
2019-01-06 14:31:04 +0100

    Basic react stuff

--- bi-react/src/index.js
+++ bi-react/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import NavBar from './NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<NavBar />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
